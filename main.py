afaa56889713627ad98fdf938718f44cac68984d
Jacob Gårder <jacob@garder.se>
2019-12-19 17:56:45 +0100

    accepting fondlist as argument etc

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
+import sys
+
 from Fund import Fund
 from TAA import TAA
 
-fundListPath = input( "Enter path to fundlist: " )
+# Fondlista som argument
+if len(sys.argv) > 1:
+    fundListPath = str(sys.argv[1])
+else:
+    fundListPath = input("Enter path to fundlist: ")
 
-with open( fundListPath, 'r' ) as handle:
+with open(fundListPath, "r") as handle:
     fundList = []
-    print( "Fetching data", end='', flush=True )
+    print("Fetching data", end="", flush=True)
 
     for line in handle:
-        if line == '\n' or line.startswith( '#' ):
+        if line == "\n" or line.startswith("#"):
             continue
         parts = line.split()
-        id = parts[ 0 ]
-        name = " ".join( parts[ 1: ] )
-        
-        fundList.append( Fund( id, name ) )
-        print('.', end='', flush=True)
+        id = parts[0]
+        name = " ".join(parts[1:])
 
-fundList = sorted( fundList, key = lambda fund: fund.getAverageReturns(), reverse = True )
+        fundList.append(Fund(id, name))
+        print(".", end="", flush=True)
 
-print( "\n\n" + Fund.getFormattedHeader() )
+fundList = sorted(fundList, key=lambda fund: fund.getAverageReturns(), reverse=True)
+
+print("\n\n" + Fund.getFormattedHeader())
 
 for fund in fundList:
-    print( fund.getFormattedData() )
+    print(fund.getFormattedData())
 
 print()
-topN = int( input( "Choose top n: " ) )
 
-if topN < 1 or topN > len( fundList ):
-    topN = len( fundList )
+# Printar toplista
+# topN = int( input( "Choose top n: " ) )
 
-print( "\n" + Fund.getFormattedHeader() )
-
-for fund in fundList[ :topN ]:
-    print( fund.getFormattedData() )
+# if topN < 1 or topN > len( fundList ):
+#     topN = len( fundList )
+# print( "\n" + Fund.getFormattedHeader() )
+# for fund in fundList[ :topN ]:
+#     print( fund.getFormattedData() )
 
 unRateData = TAA.getUnRateData()
 
-print( "\n\n=== US Unemployment rate ===" )
-print( "Current: " + str( unRateData[ 0 ] ) )
-print( "MA12: " + str( '{:.3f}'.format( unRateData[ 1 ] ) ) )
+print("\n\n=== US Unemployment rate ===")
+print("Current: " + str(unRateData[0]))
+print("MA12: " + str("{:.3f}".format(unRateData[1])))
 
-input( "\nDone" )
+print("\nDone")
